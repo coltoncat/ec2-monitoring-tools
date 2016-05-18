4e5ba7d0311ca9bf1b9d6e412e21dc750ce894a4
Max C <clementsm@amazon.com>
2016-05-18 07:24:16 +0000

    Fixed DiskFreeBytes so we get bytes not 1kB blocks

--- bin/metrics.sh
+++ bin/metrics.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-df /dev/xvda1 --output=avail | awk 'NR==2 {print}' | xargs aws cloudwatch put-metric-data --metric-name FreeDiskBytes --namespace maxclem --dimensions InstanceID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id) --timestamp $(date -u +"%Y-%m-%dT%H:%M:%SZ") --region $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/ | sed 's/.$//') --value
+df /dev/xvda1 --output=avail | awk 'NR==2 { bytes = $0 * 1024; print bytes }' | xargs aws cloudwatch put-metric-data --metric-name FreeDiskBytes --namespace maxclem --dimensions InstanceID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id) --timestamp $(date -u +"%Y-%m-%dT%H:%M:%SZ") --region $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/ | sed 's/.$//') --value
 free -bo | awk 'NR==2 {print $4}' | xargs aws cloudwatch put-metric-data --metric-name FreeMemoryBytes --namespace maxclem --dimensions InstanceID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id) --timestamp $(date -u +"%Y-%m-%dT%H:%M:%SZ") --region $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/ | sed 's/.$//') --value
 
