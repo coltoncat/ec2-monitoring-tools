4f1ec51ba838275dba251baec92afe26f019084a
Max C <clementsm@amazon.com>
2016-05-18 04:30:22 +0000

    Initial commit of metrics.sh

---
+++ metrics.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+df /dev/xvda1 --output=avail | awk 'NR==2 {print}' | xargs aws cloudwatch put-metric-data --metric-name FreeDiskBytes --namespace maxclem --dimensions InstanceID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id) --timestamp $(date -u +"%Y-%m-%dT%H:%M:%SZ") --region $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/ | sed 's/.$//') --value
+free -bo | awk 'NR==2 {print $4}' | xargs aws cloudwatch put-metric-data --metric-name FreeMemoryBytes --namespace maxclem --dimensions InstanceID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id) --timestamp $(date -u +"%Y-%m-%dT%H:%M:%SZ") --region $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/ | sed 's/.$//') --value
+
