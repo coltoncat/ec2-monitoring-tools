69f0307be311ae648d4dd93168b8da05de8eec95
Max C <clementsm@amazon.com>
2016-05-23 23:07:35 +0000

    Moved stuff around a bit, and added load-average

--- bin/metrics.sh
+++ bin/metrics.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
-df /dev/xvda1 --output=avail | awk 'NR==2 { bytes = $0 * 1024; print bytes }' | xargs aws cloudwatch put-metric-data --metric-name FreeDiskBytes --namespace maxclem --dimensions InstanceID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id) --timestamp $(date -u +"%Y-%m-%dT%H:%M:%SZ") --region $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/ | sed 's/.$//') --value
-free -bo | awk 'NR==2 {print $4}' | xargs aws cloudwatch put-metric-data --metric-name FreeMemoryBytes --namespace maxclem --dimensions InstanceID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id) --timestamp $(date -u +"%Y-%m-%dT%H:%M:%SZ") --region $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/ | sed 's/.$//') --value
-
+namespace="maxclem/ec2"
+InstanceID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+processors=$(grep -c processor /proc/cpuinfo)
+df /dev/xvda1 --output=avail | awk 'NR==2 { bytes = $0 * 1024; print bytes }' | xargs aws cloudwatch put-metric-data --metric-name FreeDiskBytes --namespace $namespace --dimensions InstanceID=$InstanceID --timestamp $(date -u +"%Y-%m-%dT%H:%M:%SZ") --region $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/ | sed 's/.$//') --value
+free -bo | awk 'NR==2 {print $4}' | xargs aws cloudwatch put-metric-data --metric-name FreeMemoryBytes --namespace $namespace --dimensions InstanceID=$InstanceID --timestamp $(date -u +"%Y-%m-%dT%H:%M:%SZ") --region $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/ | sed 's/.$//') --value
+aws cloudwatch put-metric-data --metric-name LoadAverage --namespace $namespace --dimensions InstanceID=$InstanceID --timestamp $(date -u +"%Y-%m-%dT%H:%M:%SZ") --region $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/ | sed 's/.$//') --value $(awk -v ncpu="$processors" '{normalizedLoadAvg = $1 / ncpu; print normalizedLoadAvg}' /proc/loadavg)
