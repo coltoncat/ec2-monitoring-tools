31ea171b019fa3e1d7e5ecc7d822b240051c5348
Max C <clementsm@amazon.com>
2016-05-24 05:41:28 +0000

    Updated to use /proc directly for memory

--- bin/metrics.sh
+++ bin/metrics.sh
@@ -6,5 +6,5 @@ region=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-
 processors=$(grep -c processor /proc/cpuinfo)
 
 aws cloudwatch put-metric-data --metric-name FreeDiskBytes --namespace $namespace --dimensions InstanceID=$InstanceID,Volume="/dev/xvda1" --timestamp $(date -u +"%Y-%m-%dT%H:%M:%SZ") --region $region --unit Bytes--value $(df /dev/xvda1 --output=avail | awk 'NR==2 { bytes = $0 * 1024; print bytes }')
-aws cloudwatch put-metric-data --metric-name FreeMemoryBytes --namespace $namespace --dimensions InstanceID=$InstanceID --timestamp $(date -u +"%Y-%m-%dT%H:%M:%SZ") --region $region --unit Bytes --value $(free -bo | awk 'NR==2 {print $4}')
+aws cloudwatch put-metric-data --metric-name FreeMemoryBytes --namespace $namespace --dimensions InstanceID=$InstanceID --timestamp $(date -u +"%Y-%m-%dT%H:%M:%SZ") --region $region --unit Kilobytes --value $(grep MemAvailable /proc/meminfo | grep -Eo '[0-9]+')
 aws cloudwatch put-metric-data --metric-name LoadAverage --namespace $namespace --dimensions InstanceID=$InstanceID --timestamp $(date -u +"%Y-%m-%dT%H:%M:%SZ") --region $region --value $(awk -v ncpu="$processors" '{normalizedLoadAvg = $1 / ncpu; print normalizedLoadAvg}' /proc/loadavg)
