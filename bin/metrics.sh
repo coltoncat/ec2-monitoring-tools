52c00630980e2436edaeb261e4079db66aa0a66e
Clements <maxclem@amazon.com>
2017-03-22 14:27:03 -0700

    Small change to timestamp

--- bin/metrics.sh
+++ bin/metrics.sh
@@ -4,7 +4,8 @@ namespace="maxclem/ec2"
 InstanceID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 region=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/ | sed 's/.$//') 
 processors=$(grep -c processor /proc/cpuinfo)
+timestamp=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
-aws cloudwatch put-metric-data --metric-name FreeDiskBytes --namespace $namespace --dimensions InstanceID=$InstanceID,Volume="/dev/xvda1" --timestamp $(date -u +"%Y-%m-%dT%H:%M:%SZ") --region $region --unit Bytes--value $(df /dev/xvda1 --output=avail | awk 'NR==2 { bytes = $0 * 1024; print bytes }')
-aws cloudwatch put-metric-data --metric-name FreeMemoryBytes --namespace $namespace --dimensions InstanceID=$InstanceID --timestamp $(date -u +"%Y-%m-%dT%H:%M:%SZ") --region $region --unit Kilobytes --value $(grep MemAvailable /proc/meminfo | grep -Eo '[0-9]+')
-aws cloudwatch put-metric-data --metric-name LoadAverage --namespace $namespace --dimensions InstanceID=$InstanceID --timestamp $(date -u +"%Y-%m-%dT%H:%M:%SZ") --region $region --value $(awk -v ncpu="$processors" '{normalizedLoadAvg = $1 / ncpu; print normalizedLoadAvg}' /proc/loadavg)
+aws cloudwatch put-metric-data --metric-name FreeDiskBytes --namespace $namespace --dimensions InstanceID=$InstanceID,Volume="/dev/xvda1" --timestamp $timestamp --region $region --unit Bytes--value $(df /dev/xvda1 --output=avail | awk 'NR==2 { bytes = $0 * 1024; print bytes }')
+aws cloudwatch put-metric-data --metric-name FreeMemoryBytes --namespace $namespace --dimensions InstanceID=$InstanceID --timestamp $timestamp --region $region --unit Kilobytes --value $(grep MemAvailable /proc/meminfo | grep -Eo '[0-9]+')
+aws cloudwatch put-metric-data --metric-name LoadAverage --namespace $namespace --dimensions InstanceID=$InstanceID --timestamp $timestamp --region $region --value $(awk -v ncpu="$processors" '{normalizedLoadAvg = $1 / ncpu; print normalizedLoadAvg}' /proc/loadavg)
